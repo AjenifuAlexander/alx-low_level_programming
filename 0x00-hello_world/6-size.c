7593708d9f95fed7f9dc7c44a2f410e561030a3e
AjenifuAlexander <harlextom00@gmail.com>
2023-03-16 19:17:45 -0700

    My C - Hello, World.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry Piont
+ *
+ * Description: using sizeof to print the size of various types
+ *
+ * Return 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n" ,sizeof(char));
+        printf("Size of an int: %lu bytes(s)\n" sizeof(int));
+	printf("Size of a long int: %ln bytes(s)\n" sizeof(long int));
+	printf("Size of a long long int: %ln bytes(s)\n" sizeof(long long int));
+	printf("Size of a float: %ln bytes(s)\n" sizeof(float));
+	return (0);
+}	
