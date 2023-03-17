a1e78bb2d5e3fceaccb5cc608f056d9bbe896245
AjenifuAlexander <harlextom00@gmail.com>
2023-03-17 03:11:58 -0700

    My C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,17 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Enrty point
  *
  * Description: Print a code using right function ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return 1 (Error).
+ * Return 0 (Success).
 */
 
 
 int mani(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
-
 }
