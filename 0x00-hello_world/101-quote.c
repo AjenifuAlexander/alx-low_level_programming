8d3397254ca1d4680478bc2f58cfb767770ee166
AjenifuAlexander <harlextom00@gmail.com>
2023-03-16 20:41:17 -0700

    My C - Hello, World.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description: Print a code using right function ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return 1 (Error).
+ *
+*/
+
+
+int mani(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+
+}
