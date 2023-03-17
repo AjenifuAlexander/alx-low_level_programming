346c55dd7f88c34b1aea5f2d685bf1b2667b853a
AjenifuAlexander <harlextom00@gmail.com>
2023-03-16 19:27:09 -0700

    My C - Hello, World.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  Entry Piont
+ * main -  Entry piont
  *
  * Description: using sizeof to print the size of various types
  *
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu bytes(s)\n" ,sizeof(char));
-        printf("Size of an int: %lu bytes(s)\n" sizeof(int));
-	printf("Size of a long int: %ln bytes(s)\n" sizeof(long int));
-	printf("Size of a long long int: %ln bytes(s)\n" sizeof(long long int));
-	printf("Size of a float: %ln bytes(s)\n" sizeof(float));
+	printf("Size of a char: %lu bytes(s)\n",sizeof(char));
+        printf("Size of an int: %lu bytes(s)\n",sizeof(int));
+	printf("Size of a long int: %ln bytes(s)\n",sizeof(long int));
+	printf("Size of a long long int: %ln bytes(s)\n",sizeof(long long int));
+	printf("Size of a float: %ln bytes(s)\n",sizeof(float));
 	return (0);
 }	
