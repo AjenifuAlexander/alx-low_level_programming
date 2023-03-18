ae900c0b10b3dddd729c60e52f64fa3910e81107
AjenifuAlexander <harlextom00@gmail.com>
2023-03-17 18:23:57 -0700

    My C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,12 @@
 /**
  * main - Entry point
  *
- * Description: 'A program that prints the alphabet in lowercase.'
  *
  * Return: Always 0 (Success)
  *
 */
 	int main(void)
 {
-		char i;
 
 		int lowerCase = 'a';
 
