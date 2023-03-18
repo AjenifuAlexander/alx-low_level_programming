1e1c1bec83afeb7c035cb42be0ed3b503da8b7de
AjenifuAlexander <harlextom00@gmail.com>
2023-03-18 06:40:32 -0700

    My C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 	int i;
 	int j;
 
-		for (i = 48; i <= 57; i++)
+		for (i = 0; i <= 10; i++)
 		{
-			for (j = 48; j <= 57; j++)
+			for (j = 0; j <= 10; j++)
 			{
 				if (i < j && i != j)
 				{
