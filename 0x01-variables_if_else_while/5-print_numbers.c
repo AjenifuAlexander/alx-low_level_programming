8456f190e13460ba2b502082f488efbbe7994307
AjenifuAlexander <harlextom00@gmail.com>
2023-03-18 03:46:16 -0700

    My C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry Point
+  * Description: 'Prints number of base ten'
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf('\n');
+	return (0);
+}
