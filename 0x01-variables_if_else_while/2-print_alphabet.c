eb1719ec1029a65fb12e696853c9d9d010ce45bc
AjenifuAlexander <harlextom00@gmail.com>
2023-03-17 17:10:41 -0700

    My C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ *
+ * main - Entry point
+ *
+ * Description: A program that prints the alphabet in lowercase.
+ *
+ * Return: 0 (Success)
+ *
+*/
+	int main(void)
+{
+	int i
+		char c;
+	c = 'a';
+
+	while
+		(c <= 'z') {
+		       putchar(c);
+		       c++;		}
+	putchar ("\n");
+	Return(0)
+
+}
