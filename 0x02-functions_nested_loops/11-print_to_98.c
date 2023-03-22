eb31fb1ef110a6672aff2601863bb531b29ecee3
AjenifuAlexander <harlextom00@gmail.com>
2023-03-21 20:16:33 -0700

    C - functions and nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_to_98 - 'Prints all natural numbers from n to 98, foolewd by a new line.
- * @n: print from this number
+ * print_to_98 - 'Prints numbers from n to 98, separated by comma,
+ * followed by space and number shold be printed in order
+ * @n: input
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int x;
 
-	if (n <= 98)
-	{
-		for  (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-			{
-
+	if (n > 98)
+		for (x = n; x > 98; x--)
+			printf("%d", x);
+	else
+		for (x = n; n < 98; x++)
+			printf("%d", x);
+				printf("98\n");
+}
