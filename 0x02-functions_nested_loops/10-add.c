a1afe523ff49be30ec14e962f1fb4b766e705300
AjenifuAlexander <harlextom00@gmail.com>
2023-03-21 18:21:07 -0700

    C - functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - 'Adding two intergers and returning results'
+ * @f: an integer to be added.
+ * @g: an integer to be added.
+ *
+ * Return: sum of f and g.
+ */
+int add(int f, int g)
+{
+	int c;
+
+	c = f + g;
+
+	return (c);
+}
