3937191bb8013b9d1afe1e6e864143b173a49d9f
Ajenifuja Ayotomiwa Alexander <126909438+AjenifuAlexander@users.noreply.github.com>
2023-05-03 07:45:58 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->next = new;
 			return (new);
 		}
-    		else
+		else
 			temp = temp->next;
 	}
 	return (NULL);
